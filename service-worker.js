1d3a5945b0069e5872f9e2895ecf7b867969441a
Repirahma <165306852+Repirahma@users.noreply.github.com>
2025-07-26 00:56:42 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,14 @@
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open('absensi-v1').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json'
-      ]);
+    caches.open("absensi-v1").then((cache) => {
+      return cache.addAll(["./", "./index.html", "./manifest.json"]);
     })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(res => {
+    caches.match(event.request).then((res) => {
       return res || fetch(event.request);
     })
   );
